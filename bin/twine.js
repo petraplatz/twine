844fcfae896447ef5cc5dfc8367b49af7a1e1cc2
riccardo.petracchini <riccardo.petracchini@ynap.com>
2019-10-01 22:46:25 +0200

    adding a simple credential manager

--- bin/twine.js
+++ bin/twine.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Hello cippa');
+const CredentialManager = require('../lib/credential-manager')
+
+async function main() {
+    const creds = new CredentialManager('twine')
+    let [key, secret] = await creds.getKeyAndSecret()
+
+    console.log(key, secret);
+}
+
+main().catch(console.error)
